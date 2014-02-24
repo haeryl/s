96ea1d2069e7fa7a991dd0c08fc587ce202ad136
ghost <haeryl@gmail.com>
2014-02-24 10:27:00 +0800

    tgk
    te

---
+++ index.php
@@ -0,0 +1,25 @@
+<!--
+	Nama Pengaturcara	: Mohd. Hafidz Bin Abdul Kadir
+	Tujuan	Aturcara	: Memaparkan laman utama Sistem Pendaftaran Klien Kaunseling Perkhidmatan Awam
+	Tarikh Cipta		: 8 Januari 2014
+	Tarikh Kemaskini 	: -
+-->
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en-US"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en-US"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en-US"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en-US"> <!--<![endif]-->
+	<head>
+		<?php include "php/99_head_container.php"; ?>
+	</head>
+	<body onload="goforit()" class="home page page-template page-template-page-custom-php siteorigin-panels wpb-js-composer js-comp-ver-3.5.5 vc_responsive" >
+		<div id="change_wrap_div" class="  ">
+			<?php include "php/99_hiding_menu.php"; ?>
+			<?php //include "php/99_dropdown_menu.php"; ?>
+			<?php include "php/99_sliding_menu.php"; ?>
+			<?php //include "php/99_prettyphoto_info.php"; ?>
+			<?php include "php/99_footer_info.php"; ?>
+			<a href="#" id="linkTop" class="backtotop"></a>
+		</div>
+	</body>
+</html>
